1ddcf90128f8bb04cf31a8229a2117efc3d5dee2
m2wasabi <m2wasabi@gmail.com>
2015-05-31 18:04:58 +0900

    chapter-1-2 フレームレートを計測しよう ～jThree.update()の割り込み処理～

--- chapter-1-2/index.js
+++ chapter-1-2/index.js
@@ -1,7 +1,16 @@
 jThree( function( j3 ) {
     $( "#loading" ).remove();
     j3.Trackball();
+
+    // フレームレート計測
+    var fps = 0;
+    var field_fps = $("#stat-fps");
+    j3.update(function(d){
+        fps = Math.floor(1000 / d);
+        field_fps.html('' + fps + ' fps');
+    });
+
 },
 function() {
     alert( "This browser does not support WebGL." );
-} );
+});
