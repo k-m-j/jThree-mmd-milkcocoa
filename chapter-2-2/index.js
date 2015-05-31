78cdbc0485a63944a34a669fc81a7b8b2223127f
m2wasabi <m2wasabi@gmail.com>
2015-05-31 18:50:37 +0900

    chapter-2-2  begin ソース引継ぎ

---
+++ chapter-2-2/index.js
@@ -0,0 +1,29 @@
+jThree( function( j3 ) {
+    $( "#loading" ).remove();
+    j3.Trackball();
+    j3.MMD.play(true);
+
+    // MMDのTHREE.jsオブジェクト取得
+    var obj3d = j3("mmd").three(0);
+    console.log(obj3d);
+
+    // フレームレート計測
+    var fps = 0;
+    var field_fps = $("#stat-fps");
+    j3.update(function(d){
+        fps = Math.floor(1000 / d);
+        field_fps.html('' + fps + ' fps');
+    });
+
+    // モーション制御ボタンの挙動
+    $("#btn-play").click(function (){
+        j3.MMD.play(true);
+    });
+    $("#btn-stop").click(function (){
+        j3.MMD.pause();
+    });
+
+},
+function() {
+    alert( "This browser does not support WebGL." );
+});
