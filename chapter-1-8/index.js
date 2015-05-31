4b1eada4787f2f8d564fe8f61a0af5b93e00b5ad
m2wasabi <m2wasabi@gmail.com>
2015-05-31 18:20:34 +0900

    chapter-1-8  begin ソース引継ぎ

---
+++ chapter-1-8/index.js
@@ -0,0 +1,25 @@
+jThree( function( j3 ) {
+    $( "#loading" ).remove();
+    j3.Trackball();
+    j3.MMD.play();
+
+    // フレームレート計測
+    var fps = 0;
+    var field_fps = $("#stat-fps");
+    j3.update(function(d){
+        fps = Math.floor(1000 / d);
+        field_fps.html('' + fps + ' fps');
+    });
+
+    // モーション制御ボタンの挙動
+    $("#btn-play").click(function (){
+        j3.MMD.play();
+    });
+    $("#btn-stop").click(function (){
+        j3.MMD.pause();
+    });
+
+},
+function() {
+    alert( "This browser does not support WebGL." );
+});
