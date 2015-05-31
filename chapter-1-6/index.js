504dd06accf9891dc135611aabe5eeffd1e04394
m2wasabi <m2wasabi@gmail.com>
2015-05-31 18:15:14 +0900

    chapter-1-6  begin ソース引継ぎ

---
+++ chapter-1-6/index.js
@@ -0,0 +1,17 @@
+jThree( function( j3 ) {
+    $( "#loading" ).remove();
+    j3.Trackball();
+    j3.MMD.play();
+
+    // フレームレート計測
+    var fps = 0;
+    var field_fps = $("#stat-fps");
+    j3.update(function(d){
+        fps = Math.floor(1000 / d);
+        field_fps.html('' + fps + ' fps');
+    });
+
+},
+function() {
+    alert( "This browser does not support WebGL." );
+});
