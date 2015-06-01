c1201a83a38a8821ed715749adbeab4a71a90316
m2wasabi <m2wasabi@gmail.com>
2015-06-01 19:27:22 +0900

    chapter-5-5 Kinectの関節情報をmmdモデルに適用する5 ～長時間更新されないモデルを非表示にする～

--- chapter-5-5/index.js
+++ chapter-5-5/index.js
@@ -1,12 +1,17 @@
 jThree( function( j3 ) {
 
+    // 消すまでのコマ数
+    var vanishFrames = 52;
     $( "#loading" ).remove();
     j3.Trackball();
 
     // jThreeのMMDオブジェクト取得
     var models = [];
+    var mmdtag = [];
+    var lastUpdate = [];
     for(var _bodyIndex = 0; _bodyIndex < 6 ; _bodyIndex++){
         models[_bodyIndex] = j3.Kinect( "mmd#obj-zunko-" + _bodyIndex);
+        mmdtag[_bodyIndex] = j3( "mmd#obj-zunko-" + _bodyIndex);
     }
 
     // モーション更新関数
@@ -16,14 +21,27 @@ jThree( function( j3 ) {
             j3.update(arguments.callee, false);
             return;
         }
+        update(motionData[i]);
+    });
 
+    function update(data){
         // bodyIndex取得
-        var bodyIndex = motionData[i].bodyIndex;
+        var bodyIndex = data.bodyIndex;
+        // 最終更新コマを記録
+        lastUpdate[bodyIndex] = i;
+        mmdtag[bodyIndex].css("display",true);
         // jointデータ取得
-        var joints = motionData[i++].joint;
+        var joints = data.joint;
         // モーション更新
         models[bodyIndex].update( joints );
-    });
+        // 古いフレームを非表示にする
+        for(var _bodyIndex = 0; _bodyIndex < 6 ; _bodyIndex++) {
+            if(i - lastUpdate[_bodyIndex] > vanishFrames){
+                mmdtag[_bodyIndex].css("display",false);
+            }
+        }
+        i++;
+    }
 
 },
 function() {
