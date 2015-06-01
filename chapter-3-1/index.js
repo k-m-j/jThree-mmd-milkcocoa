8a5566c37cff8f08eec8696b4a2d814e727a868b
m2wasabi <m2wasabi@gmail.com>
2015-06-01 14:07:14 +0900

    chapter-3-1 MMDモデルを自由に操る1 ～JavaScriptでのmeshオブジェクトの表示～

--- chapter-3-1/index.js
+++ chapter-3-1/index.js
@@ -1,7 +1,15 @@
-jThree( function( j3 ) {
-    $( "#loading" ).remove();
-    j3.Trackball();
-},
-function() {
-    alert( "This browser does not support WebGL." );
-});
+jThree(
+    function( j3 ) {
+        $( "#loading" ).remove();
+        j3.Trackball();
+
+        var boxes = "";
+        for(var i = 0; i < 10 ; i++){
+            boxes += '<mesh id="pos-' + i + '" class="cubes" geo="#geo-cube" mtl="#mtl-blue" style="positionX:' + i*2 + ';"><mesh id="dir-' + i + '" geo="#geo-corn" mtl="#mtl-blue" style="positionY:0.6;"></mesh></mesh>';
+        }
+        j3("scene").append(boxes);
+    },
+    function() {
+        alert( "This browser does not support WebGL." );
+    }
+);
